b07faec55d2d32bc97546805f63cd116ae49066e
giuseppe <giuseppe@Mac-Pro-di-Giuseppe.local>
2014-03-02 14:31:27 +0100

    remove neo4jData Folder add to shell.sh script curl -O neo4jData in dropbox add stop/restart option in shell.sh

--- script.sh
+++ script.sh
@@ -6,17 +6,20 @@ BOA_ROOT=$PWD
 BOA_SERVER="$BOA_ROOT/BusOnAirServer"
 BOA_WEB="$BOA_ROOT/BusOnAirWeb"
 NEO4J_HOME="$BOA_ROOT/neo4j"
+$NEO4J_DATA="$BOA_ROOT/neo4jData"
 NEO4J_VERSION="1.8.M03"
 export MAVEN_OPTS=-Xmx512m 
 NEO4J_PIDFILE="$NEO4J_HOME/neo4j-service.pid"
 
 while true; do
+    clear
     cd $BOA_ROOT
     echo "
         1   Compila BoaServer
         2   Inizializza neo4j
         3   Importa i dati
         4   Avvia il BoaWebServer
+        5   Stop BoaWebServer
     "
     echo -n "Fai la tua scelta: "
     read INPUT # Read user input and assign it to variable INPUT
@@ -28,14 +31,23 @@ while true; do
         pause 'Press [Enter] key to continue...'
         ;;
     2)  
-        if [ -f $NEO4J_PIDFILE ]; then PID=`cat $NEO4J_PIDFILE`; else PID=0; fi
+        if [ -f $NEO4J_PIDFILE ]
+        then 
+            PID=`cat $NEO4J_PIDFILE`
+        else 
+            PID=0
+        fi
  
         if [ ! -d $NEO4J_HOME ]
         then
             curl http://dist.neo4j.org/neo4j-community-$NEO4J_VERSION-unix.tar.gz | tar xvzf -
             mv neo4j-community-$NEO4J_VERSION neo4j
         fi
-        
+        if [ ! -d $NEO4J_DATA ]
+        then
+            curl https://dl.dropboxusercontent.com/s/yifbg9ycucjgwg0/neo4jData.tar.gz | tar xvzf -
+        fi
+
         if ps -p $PID > /dev/null
         then
             echo "$PID is running"
@@ -56,6 +68,7 @@ while true; do
         sudo $NEO4J_HOME/bin/neo4j start
         pause 'Press [Enter] key to continue...'
         ;;
+
     3)  
         cd ./import
         ./testImporter.sh
@@ -64,11 +77,35 @@ while true; do
         ;;
     4) 
         cd $BOA_WEB
+        
+        if ps -x | grep  [a]pp.js | wc -l > /dev/null
+        then
+          echo "Web Server is running"
+          ps -x | grep -V | grep [a]pp.js | xargs kill -9
+          echo "Web Server install OFF"
+        else
+          echo "Web Server is not running"
+        fi
         npm install 
-        node app.js &
+        node app.js&
+            echo "Web Server is ON"
         cd $BOA_ROOT
         pause 'Press [Enter] key to continue...'
        ;;
+
+    5)
+        cd $BOA_WEB
+        if ps -x | grep  [a]pp.js | wc -l > /dev/null
+        then
+          echo "Web Server is running"
+          ps -x | grep -V | grep [a]pp.js | xargs kill -9
+          echo "Web Server install OFF"
+        else
+          echo "Web Server is not running"
+        fi
+        pause 'Press [Enter] key to continue...'
+       ;;
+
     *) echo "opzione non valida"
         pause 'Press [Enter] key to continue...'
        ;;
